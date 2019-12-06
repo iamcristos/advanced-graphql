63338f0f3d3781eff4d037a7d07efdfd4690de28
icpengineer <icpengineer@intcrzyppl.com>
2019-12-06 17:08:19 +0100

    ..

--- src/api/project/project.resolvers.js
+++ src/api/project/project.resolvers.js
@@ -1,6 +1,22 @@
 // resolve the query you made
+const Project = require('./project.model');
+
+const project = (_,{id}) => Project.findById(id).exec();
+
+const projects = () => Project.find({}).exec();
+
+const newProject = (_, {input}) => Project.create(input);
+
+const updateProject = (_, {id, input}) => Project.findByIdAndUpdate(id, input, {new: true}).exec()
+
 module.exports = {
   Query: {
-    
+    project,
+    projects
+  },
+
+  Mutation: {
+    newProject,
+    updateProject
   }
 }
