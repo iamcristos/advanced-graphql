eca554e6ccee0f81368eef1176de5e1dd9b10b82
icpengineer <icpengineer@intcrzyppl.com>
2019-12-11 23:46:20 +0100

    wrote unit and integration test

--- tests/api/project.spec.js
+++ tests/api/project.spec.js
@@ -13,10 +13,34 @@ describe('Project', () => {
     describe('project', () => {
       test('should resolve correctly', async () => {
         // test the resolver function
+        await db.models.project.create(
+          {name: 'test', description: 'testing'},
+          // {name: 'test2'}, {description: 'testing2'}
+        )
+        const projectResult = await projectResolvers.Query.projects({}, {}, {
+          models: {
+            project: db.models.project
+          }
+        })
+
+        expect(projectResult).toHaveLength(1)
       })
 
       test('should have correct query', async () => {
         // run an actual query and test result
+        const query = `
+          query {
+            projects {
+              id
+              name
+            }
+
+          }
+        `;
+
+        const result = await runQuery(query)
+
+        expect(result.errors).toBeUndefined()
       })
     })
   })
